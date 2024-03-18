33d97de01155a7a87d0b7fbc997c63616d2d9f70
Christian Larsen <christi9nl9rsen@gmail.com>
2024-03-18 13:45:18 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Services from "./pages/Services"
-import NoPage from "./pages/NoPage"
-import { BrowserRouter, Routes, Route }from 'react-router-dom';
-import Navbar from "./components/Navbar"
-import Website from "./pages/Website";
-import PCBuilds from "./pages/PCBuilds";
-import Hosting from "./pages/Hosting";
+
 
 function App(){
 
   return(
     <>
-      <Navbar />
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />}/>
-          <Route path='/home' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/website" element={<Website />}/>
-          <Route path="/pcbuilds" element={<PCBuilds />}/>
-          <Route path="/hosting" element={<Hosting />}/>
-          <Route path='/*' element={<NoPage />} />
-        </Routes>
-      </BrowserRouter>
     </>
   );
 
